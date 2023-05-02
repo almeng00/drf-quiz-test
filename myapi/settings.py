750ea6b06edbae8f7bd9332eeff600cdde2a0748
Dan Kim <padadak20@naver.com>
2023-05-02 19:56:16 +0900

    third

--- myapi/settings.py
+++ myapi/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 from pathlib import Path
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,8 +88,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url(conn_max_age=500)
-DATABASES['default'].update(db_from_env )
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
