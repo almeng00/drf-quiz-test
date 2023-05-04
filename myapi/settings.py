cc9ca889bd086771d88d67a5fa2fad3f83211e74
zinco <oay2319@naver.com>
2023-05-04 14:22:01 +0900

    Ready for deploy

--- myapi/settings.py
+++ myapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-npdx1ve=s)6h6#clf$gen3npv0#(4qhiay+@h+y77q+l)8^)52')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-npdx1ve=s)6h6#clf$gen3npv0#(4qhiay+@h+y77q+l)8^)52')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
